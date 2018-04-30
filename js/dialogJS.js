df928e61e3bad1049c1175ec8ed513c309d808a8
paul911014 <paul911014@naver.com>
2018-04-30 12:00:13 +0900

    su

---
+++ js/dialogJS.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+  var sendModalBtn = document.getElementByID('btn_send');
+  var sendBtns = document.querySelectorAll('.send');
+  var sendCloseBtns = document.querySelectorAll('.send_form_close');
+}
